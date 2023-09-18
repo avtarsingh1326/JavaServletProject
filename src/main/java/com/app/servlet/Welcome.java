8e15bb3254b47f8b8bd5b725a23815d6085e7bf9
asingh0711 <139422827+asingh0711@users.noreply.github.com>
2023-09-17 21:56:03 -0400

    first commit

---
+++ src/main/java/com/app/servlet/Welcome.java
@@ -0,0 +1,58 @@
+package com.app.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Welome
+ */
+
+public class Welcome extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Welcome() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html>\n"
+				+ "<head>\n"
+				+ "<title>Page Title</title>\n"
+				+ "</head>\n"
+				+ "<body>\n"
+				+ "\n"
+				+ "<h1>This is a Heading</h1>\n"
+				+ "<p>This is a paragraph.</p>\n"
+				+ "\n"
+				+ "</body>\n"
+				+ "</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
